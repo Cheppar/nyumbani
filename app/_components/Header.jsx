8501c90e69c38e0921b25abe862a34ddb0451400
makenaejiofor <ejioforched@gmail.com>
2024-07-09 16:44:26 +0300

    app before authentication

---
+++ app/_components/Header.jsx
@@ -0,0 +1,86 @@
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "@radix-ui/react-icons";
+import { Badge } from "@/components/ui/badge";
+import { Switch } from "@/components/ui/switch"
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
+
+
+
+
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+function Header() {
+  const path = usePathname();
+
+  useEffect(() => {
+    console.log(path);
+  }, []);
+
+  return (
+    <div className="p-6 px-10 flex justify-between shadow-sm fixed top-0 w-full z-10 bg-white">
+      <div className="flex gap-10 items-center">
+        <Image src={"/logo.svg"} width={50} height={50} alt="logo" />
+
+        <ul className="hidden md:flex gap-10">
+          <Link href={"/"}>
+            {" "}
+            <li
+              className={`'hover:text-primary font-medium text-sm cursor-pointer'  
+        ${path == "/" && "text-primary"}`}
+            >
+              For Sale
+            </li>
+          </Link>
+
+          <Link href={"/rent"}>
+            <li className="hover:text-primary font-medium text-sm cursor-pointer">
+              For Rent
+            </li>
+          </Link>
+
+          <Link href={"/agent"}>
+            <li className="hover:text-primary font-medium text-sm cursor-pointer">
+              Agent Finder
+            </li>
+          </Link>
+        </ul>
+      </div>
+
+      <div>
+        <ul className="hidden md:flex gap-10">
+          <Link href={"/"}>
+            {" "}
+            <li className="mt-2 hover:text-primary font-medium text-sm cursor-pointer">
+              <Badge>Indoor Maps</Badge>
+            </li>
+          </Link>
+        </ul>
+      </div>
+
+      <div className="flex gap-2">
+        <Button className="flex gap-2">
+          <PlusIcon className="h-5 w-5" /> Post an Ad
+        </Button>
+        <Button variant="outline">Login</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
